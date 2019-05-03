4d55755d2d47af78262013efcba9ed1866b62a01
Rogelio Martinez <rogeliomm@live.com>
2019-05-03 18:54:40 -0500

    OG boss movement and first shot verision

---
+++ src/better/enemies/OGBoss.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package better.enemies;
+
+import better.assets.Assets;
+import better.core.Game;
+import better.core.Timer;
+import better.core.Util;
+import better.game.Bullet;
+import better.game.Light2D;
+import better.game.Player;
+import better.scenes.LevelScreen;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Cesar Barraza
+ */
+public class OGBoss extends Enemy {
+    private boolean shouldRenderBar;
+    private int xSpeed;
+    private int ySpeed;
+    private boolean spawning;
+    private Timer moveTimer;
+    private Timer shootTimer;
+    private Timer stop;
+    private Timer move;
+    
+    // Level Bullet List //
+    private ArrayList<Bullet> bullets;
+    
+    public OGBoss(float width, float height, int score, int coins, int health, Player player, ArrayList<Bullet> bullets, ArrayList<Light2D> lights) {
+        super(0, 0, width, height, score, coins, health, player, lights);
+        this.bullets = bullets;
+        shouldRenderBar = false;
+        theta = 0;
+        img = Assets.images.get("EnemyShip1");
+        spawnEnemy();
+        spawning = true;
+        ySpeed = 0;
+        xSpeed = -3;
+        moveTimer = new Timer(0);
+        shootTimer = new Timer(1);
+        stop = new Timer(3);
+        move = new Timer(6);
+    }
+
+    private void spawnEnemy(){
+        int WIDTH = Game.getDisplay().getWidth();
+        int HEIGHT = Game.getDisplay().getHeight();
+        x = WIDTH + width;
+        y = 10;
+    }
+    
+    private void shoot(){
+        bullets.add(new Bullet(getX() + getWidth()*1 / 4, getY() + getHeight() / 2, 10, 10, 5,
+                    theta, 6, Assets.images.get("BulletRed"), Bullet.BULLET_TYPE_ENEMY, Color.RED, lights));
+        bullets.add(new Bullet(getX() + getWidth()*2 / 4, getY() + getHeight() / 2, 10, 10, 5,
+                    theta, 6, Assets.images.get("BulletRed"), Bullet.BULLET_TYPE_ENEMY, Color.RED, lights));
+        bullets.add(new Bullet(getX() + getWidth()*3 / 4, getY() + getHeight() / 2, 10, 10, 5,
+                    theta, 6, Assets.images.get("BulletRed"), Bullet.BULLET_TYPE_ENEMY, Color.RED, lights));
+        bullets.add(new Bullet(getX() + getWidth()*4 / 4, getY() + getHeight() / 2, 10, 10, 5,
+                    theta, 6, Assets.images.get("BulletRed"), Bullet.BULLET_TYPE_ENEMY, Color.RED, lights));
+    }
+    
+    private void move(){
+        if (player.getX() > x){
+            xSpeed += 1;
+        }
+        if (player.getX() < x){
+            xSpeed -= 1;
+        }
+       
+        if (xSpeed >= 5){
+            xSpeed = 5;
+        }
+        if (xSpeed <= -5){
+            xSpeed = -5;
+        }
+    }
+    private void checkCollision(){
+        if (y > Game.getDisplay().getHeight()/2-height){
+            ySpeed *= -1;
+            y = Game.getDisplay().getHeight()/2-height;
+        }
+        if (y < 10){
+            ySpeed *= -1;
+            y = 10;
+        }
+        if (x < 0){
+            
+            x = 0;
+        }
+        if (x > Game.getDisplay().getWidth() - width){
+            
+            x= Game.getDisplay().getWidth() - width;
+        }
+    }
+    @Override
+    public void update(){
+        super.update();
+        if (spawning){
+            setX(getX() - 3);
+            if (x <= Game.getDisplay().getWidth() - width){
+                spawning = false;
+            }
+        }else{
+            // moves the object
+            setX(getX() + xSpeed);
+            setY(getY() + ySpeed);
+            
+            if (moveTimer.isActivated()){
+                moveTimer.restart(0.1);
+                move();
+            }
+            
+            if (shootTimer.isActivated()){
+                shootTimer.restart();
+                shoot();
+            }
+            this.checkCollision();
+            
+            moveTimer.update();
+            shootTimer.update();
+            
+        }  
+        
+        // update healthbar
+        healthBar.setValue(health);
+        healthBar.setX(x);
+        healthBar.setY(y - 10);
+        
+
+    }
+    
+    @Override
+    public void render(Graphics2D g) { 
+        super.render(g);
+        
+        if(shouldRenderBar) {
+            healthBar.render(g);
+        }
+    }
+    
+    @Override
+    public Rectangle2D.Float getRect() {
+        return new Rectangle2D.Float(x + 32, y + 32, 64, 64);
+    }
+    
+    @Override
+    public void mouseEnter() {
+        shouldRenderBar = true;
+    }
+    
+    @Override
+    public void mouseLeave() {
+        shouldRenderBar = false;
+    }
+}
--- src/better/levels/Level3.java
+++ src/better/levels/Level3.java
@@ -16,6 +16,7 @@ import better.enemies.Enemy2;
 import better.enemies.Enemy3;
 import better.game.Player;
 import better.enemies.Enemy4;
+import better.enemies.OGBoss;
 import better.enemies.OGEnemy1;
 import better.enemies.OGEnemy2;
 import better.game.Powerup;
@@ -25,7 +26,7 @@ import better.game.Powerup;
  * @author Cesar Barraza
  */
 public class Level3 extends Level {
-    private static final int TO_DEFEAT = 100;
+    private static final int TO_DEFEAT = 1;
     private int defeated;
     private Timer spawnTimer;
     
@@ -42,7 +43,7 @@ public class Level3 extends Level {
         if(spawnTimer.isActivated()) {
             //enemies.add(new Asteroid1(0, 0, 128, 128, 100, 0, 10, player, bullets, lights));
             enemies.add(new OGEnemy1(64, 64, 100, 10, 40, player, bullets, lights));
-            enemies.add(new OGEnemy2(64, 64, 100, 10, 40, player, bullets, lights));
+            enemies.add(new OGEnemy2(64, 64, 100, 10, 20, player, bullets, lights));
             if(defeated < TO_DEFEAT) {
                 spawnTimer.restart(Util.randNumF(1.5f, 2.5f));
             } else {
@@ -58,9 +59,9 @@ public class Level3 extends Level {
     public void onEnemyDead(Enemy enemy) {
         defeated++;
 
-        //if(defeated == TO_DEFEAT) {
-        //    enemies.add(new Boss1(Game.getDisplay().getWidth() / 2 - 75, -300, 128, 128, 750, player, bullets, lights));
-        //}
+        if(defeated == TO_DEFEAT) {
+            enemies.add(new OGBoss(128, 128, 100, 10, 200, player, bullets, lights));
+        }
 
         // spawn powerup
         int p = Util.randNum(1, 9);
