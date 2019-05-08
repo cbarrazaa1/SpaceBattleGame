40613eafefcfb10b789325d33bdf1319a870a046
Rogelio Martinez <rogeliomm@live.com>
2019-05-08 11:06:17 -0500

    fixed bug when finishing game

--- src/better/scenes/LevelSelectScreen.java
+++ src/better/scenes/LevelSelectScreen.java
@@ -295,10 +295,14 @@ public class LevelSelectScreen extends Screen {
         
         // get unlocked planets
         for(int i = 0; i < player.getLevel(); i++) {
+            if (i >= 9) break;
             selectablePlanets.get(8 - i).setState(PlanetState.NORMAL);
         }
-        selectPlanet(selectablePlanets.get(8 - player.getLevel() + 1).getName());
-        
+        if (player.getLevel() >= 9){
+            selectPlanet("Sun");
+        }else{
+            selectPlanet(selectablePlanets.get(8 - player.getLevel() + 1).getName());
+        }
         // music
         Assets.playMusic(Assets.mainMenu);
         
